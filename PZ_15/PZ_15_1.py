697995b16b12fc7b8499e7b3ed1f1723363050e0
mariia2206 <alex.bog04@mail.ru>
2022-04-09 13:10:46 +0300

    PZ_15

---
+++ PZ_15/PZ_15_1.py
@@ -0,0 +1,18 @@
+# В матрице элементы последнего столбца заменить на -1
+
+
+import random
+n, m = [int(input(i)) for i in ('Введите кол-во столбцов: ', 'Введите кол-во строк:')]
+matrix = [[random.randint(-5, 5) for i in range(n)] for j in range(m)]
+
+print('Начальная матрица: ')
+for i in matrix:
+    print(*i)
+    
+for i in range(m):
+    matrix[i][n-1] = -1
+
+print('Полученнаня матрица: ')
+for i in matrix:
+    print(*i)
+
