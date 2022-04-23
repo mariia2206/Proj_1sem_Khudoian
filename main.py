ffe86cc517c0411c68176fa4a77ffd2baf463aca
Yurii <nais30055003@gmail.com>
2022-04-23 17:30:08 +0300

    .

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import figures
+import sqlite3 as sq
+info_users = [
+    (1, 'Алексей', 1, 22, 1000),
+    (2, 'Миша', 1, 19, 800),
+    (3, 'Сергей', 1, 19, 900),
+    (4, 'Мария', 2, 18, 1500),
+    (5, 'Александр', 1, 20, 1100),
+]
 
-print('Периметр треугольника: ', figures.triangle_perimetr())
-print('Периметр квадрата: ', figures.square_perimetr())
-print('Периметр круга: ', figures.circle_perimetr())
-print('Площадь треугольника: ', figures.triangle_area())
-print('Площадь квадарата: ', figures.square_area())
-print('Площадь круга: ', figures.circle_area())
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+    #cur.execute("DROP TABLE IF EXISTS users")
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,
+sex INTEGER NOT NULL DEFAULT 1, old INTEGER,
+score INTEGER
+)""")
---
+++ main2.py
@@ -0,0 +1,8 @@
+import figures
+
+print('Периметр треугольника: ', figures.triangle_perimetr())
+print('Периметр квадрата: ', figures.square_perimetr())
+print('Периметр круга: ', figures.circle_perimetr())
+print('Площадь треугольника: ', figures.triangle_area())
+print('Площадь квадарата: ', figures.square_area())
+print('Площадь круга: ', figures.circle_area())
